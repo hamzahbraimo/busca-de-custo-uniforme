83a4461351c1c5562ecc765ce6afb7de05ea3b5d
Hamzah Braimo <hamzah.brm@gmail.com>
2026-05-12 14:39:29 +0200

    remodelacao do projecto

--- classes/node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-    def __init__(self, name: str):
-        self.name: str = name
-        self.left: Node | None = None
-        self.right: Node | None = None
-        self.children: int = 0
--- main.py
+++
@@ -1,9 +0,0 @@
-from classes.tree import Tree
-
-av = Tree()
-av.add("A", -1) # raiz
-av.add("B", 3)
-av.add("C", 4)
-av.add("D", 5)
-
-print(av.dfs()) # listagem da arvore
