0905889ae358f5f5ea68ae4ce6f1f2b8d9104fc2
Hamzah Braimo <hamzah.brm@gmail.com>
2026-05-12 16:21:33 +0200

    Adicao de estado inicial

--- elementos/vertice.py
+++ elementos/vertice.py
@@ -3,6 +3,7 @@ class Vertice:
     def __init__(self, nome: str, objectivo: bool):
         self.nome = nome
         self.objectivo = objectivo
+        self.inicial: bool | bool = False
 
     def get_nome(self):
         return self.nome
